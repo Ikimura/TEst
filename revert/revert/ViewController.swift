b2fb671aacd03019dd70d32408c40012c34588b8
Artsiom Karseka <artsiom_karseka@epam.com>
2019-08-18 18:14:10 +0300

    feature(content): second commit feature_1

--- revert/revert/ViewController.swift
+++ revert/revert/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 	// add content feature 1
+	// add second content feature 1
     }
 
     override func didReceiveMemoryWarning() {
